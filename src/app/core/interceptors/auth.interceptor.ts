8907d8d43b4dd2a055f00ff33096878f0e78228f
Ayoub Lemrachchaq <aalayoub33@gmail.com>
2026-03-05 13:45:52 +0000

    fix auth issue

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,11 +1,8 @@
 import { HttpInterceptorFn } from '@angular/common/http';
-import { inject } from '@angular/core';
-import { AuthService } from '../services/auth.service';
 import { API_URL } from '../constants';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-    const authService = inject(AuthService);
-    const token = authService.token();
+    const token = localStorage.getItem('access_token');
 
     const isApiUrl = req.url.startsWith(API_URL);
 
